5af4cf224af803568d583d3fb12039f702bc3732
JackFilali <47795459+JackFilali@users.noreply.github.com>
2019-02-26 15:16:31 -0500

    Update datmoney (1).c

--- datmoney (1).c
+++ datmoney (1).c
@@ -1,12 +1,11 @@
+#include<cs50.h>
 #include <stdio.h>
 
 int main()
 {
     float avg = 0;
     float sum =0;
-    float earned=0;
-    float possible;
-    int x=0;
+    
     int i=0;
 
     /* Array- declaration – length 4*/
@@ -19,21 +18,29 @@ int main()
     /* We are using a for loop to traverse through the array
      * while storing the entered values in the array
      */
-    for (x=0; x<i;x++)
+    for (int x=0; x<i;x++)
     {
+        float earned[x];
+        float possible[x];
         printf("Enter earned %d \n", (x+1));
-        scanf("%f", &earned);
+        scanf("%f", &earned[x]);
         printf("Enter possible %d \n", (x+1));
-        scanf("%f", &possible);
-       num[x] = earned/possible*100;
+        scanf("%f", &possible[x]);
+       
 printf("your grade is: %f\n", num[x]);
+        
     }
-    for (x=0; x<i;x++)
+    
+    for (int x=0; x<i;x++)
     {
-        sum = sum+num[x];
+   
+        float earned[x];
+        float possible[x];
+      
+        float sumposs= sum+possible[x];
+        float sumearn= sum+earned[x];
     }
 
     avg = sum/i;
     printf("Average of entered number is: %f", avg);
     return 0;
-}
